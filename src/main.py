f24214de42a497347ac63f2e215aa45b1de5cd27
Alex Jeleniewski <alexjan100@hotmail.com>
2022-02-21 13:33:03 +0100

    wip of indexing through collection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from pyserini import collection, index
 
 if __name__ == '__main__':
-    collection = collection.Collection('HtmlCollection', 'collections/cacm/')
+    collection = collection.Collection('JsonCollection', 'data/jsonl/')
     generator = index.Generator('DefaultLuceneDocumentGenerator')
+
+    for (i, fs) in enumerate(collection):
+        for (j, doc) in enumerate(fs):
+            parsed = generator.create_document(doc)
+            docid = parsed.get('id')            # FIELD_ID
+            raw = parsed.get('raw')             # FIELD_RAW
+            contents = parsed.get('contents')   # FIELD_BODY
+            print('{} {} -> {} {}...'.format(i, j, docid, contents.strip().replace('\n', ' ')[:50]))
