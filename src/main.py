28faada2be6c6ddef6e1e32777e944a9578a564e
Alex Jeleniewski <alexjan100@hotmail.com>
2022-02-27 15:38:38 +0100

    add bounding algorithm and writing to file

--- src/main.py
+++ src/main.py
@@ -4,6 +4,16 @@ from pyserini.index import IndexReader
 from pyserini.search import SimpleSearcher
 
 
+def get_int(val):
+    if val > 0.95:
+        return 3
+    if val > 0.85:
+        return 2
+    if val > 0.7:
+        return 1
+    return 0
+
+
 def do_bm25():
     searcher = SimpleSearcher("data/indexes")
     searcher.set_bm25(1.2, 0.75)
@@ -13,16 +23,11 @@ def do_bm25():
         queries = csv.reader(query_file, delimiter="\t")
         for (qid, query) in queries:
             hits = searcher.search(query)
-            sum_score = 0
+            max_score = hits[0].score
             for hit in hits:
-                sum_score += hit.score
-
-            # for hit in hits:
-            #     y = math.ceil(hit.score / sum_score * 4.0) - 1
-            #     hit.score = y
+                y = hit.score / max_score
+                hit.score = get_int(y)
             all_hits.append((qid, hits))
-            # if len(all_hits) == 25:
-            #     break
 
     return all_hits
 
@@ -38,16 +43,8 @@ if __name__ == '__main__':
         for (qid, q0, docid, rating) in qrels:
             qrels_dict[f"{qid}{docid}"] = rating
 
-    print("rank pos/qid/docid/score_us/score_them")
-    for (qid, hits) in res:
-        rank = 0
-        for hit in hits:
-            if f"{qid}{hit.docid}" in qrels_dict:
-                print(f"{rank}/{qid}/{hit.docid}/{hit.score}/{qrels_dict[f'{qid}{hit.docid}']}")
-            rank += 1
-    # query = 'atomic'
-    # docids = range(0, 100)
-    #
-    # for i in range(0, len(docids)):
-    #     score = index_reader.compute_query_document_score(str(docids[i]), query)
-    #     print(f'{i + 1:2} {docids[i]:15} {score:.5f}')
+    with open("qrels.txt", "w", newline='') as qrel_res:
+        write = csv.writer(qrel_res, delimiter=' ')
+        for (qid, hits) in res:
+            for hit in hits:
+                write.writerow([qid, "Q0", hit.docid, int(hit.score)])
