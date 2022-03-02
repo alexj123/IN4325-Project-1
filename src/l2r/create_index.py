4063ee73bcefae0db6c150fd88ef7459f00a84f0
Alex Jeleniewski <alexjan100@hotmail.com>
2022-03-02 20:59:03 +0100

    ignore files

--- src/l2r/create_index.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pyterrier as pt
-
-pt.init()
-
-df = pd.read_csv('data/collection.tsv', delimiter="\t", names=["docno", "text"])
-indexer = pt.IterDictIndexer('D:/Projects/Uni/IN4325-Project-1/src/l2r/data/msmarco-passage-index')
-
-index_ref = indexer.index(df.to_dict(orient="records"))
